84a6dae127efb898229e8739112879fd5463ed58
chandrasekhar2039 <56759909+chandrasekhar2039@users.noreply.github.com>
2020-06-01 22:46:10 +0530

    task 2

---
+++ index.js
@@ -0,0 +1,8 @@
+function swt(){
+  document.querySelector("body").classList.toggle("dark");
+  document.querySelector("h1").classList.toggle("o");
+  document.querySelector(".btn").classList.toggle("bb");
+
+
+
+}
